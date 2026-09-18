a888d7c8803eb6840373d10399d3129cc12bbf85
r1yn0 <rayyyyan00@gmail.com>
2026-09-18 19:09:53 +0100

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'DZ ROLEPLAY'
+description 'DZ ROLEPLAY Admin Menu'
+version '1.0.0'
+
+lua54 'yes'
+
+shared_script 'config.lua'
+
+client_script 'client.lua'
+
+server_script 'server.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/app.js'
+}
+
+dependency 'qb-core'
+end
