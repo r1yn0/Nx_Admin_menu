dc8cc976f346eb8545f2e9ec34220dfc3a2ab675
r1yn0 <rayyyyan00@gmail.com>
2026-09-18 19:09:31 +0100

    config.lua

---
+++ config.lua
@@ -0,0 +1,21 @@
+Config = {}
+
+-- Command used to open the menu
+Config.OpenCommand = 'admin'
+
+-- Permissions allowed to use the menu
+Config.Permissions = {
+    'admin',
+    'god'
+}
+
+-- Menu information
+Config.ServerName = 'DZ ROLEPLAY'
+
+-- Main colors
+Config.Colors = {
+    primary = '#19c37d',
+    secondary = '#e63946',
+    background = '#101214'
+}
+end 
